b7d4d97300e36d4f27431b0a53db0a0d9e9fb040
Thomas Heritier <thomas.heritier@miles.no>
2021-02-07 18:58:00 +0100

    add king squid

---
+++ components/KingSquid.js
@@ -0,0 +1,41 @@
+import React, { useRef, useEffect, useState } from 'react';
+import Box from './Box';
+import Flex from './Flex';
+import useMousePosition from './hooks/useMousePosition';
+import useSize from './hooks/useSize';
+
+const KingSquid = () => {
+  const ref = useRef(null);
+  const size = useSize(ref);
+  const mouse = useMousePosition();
+  const [eyePosition, setEyePosition] = useState(null);
+
+  useEffect(() => {
+    if (size) {
+      const { width, height } = size;
+
+      const cx = width / 2.09;
+      const cy = height / 3;
+
+      const phi = Math.atan2(mouse.y - cy, mouse.x - cx);
+      const nx = cx + 30 * Math.cos(phi);
+      const ny = cy + 30 * Math.sin(phi);
+      setEyePosition({ x: nx, y: ny });
+    }
+  }, [mouse, size]);
+
+  return (
+    <Flex ref={ref} justify="center" pos="relative">
+      {eyePosition && (
+        <>
+          <img src="/images/squidbody.png" alt="Squid body" />
+          <Box pos="absolute" style={{ left: eyePosition.x, top: eyePosition.y }}>
+            <img src="/images/squideye.png" alt="Squid eye" />
+          </Box>
+        </>
+      )}
+    </Flex>
+  );
+};
+
+export default KingSquid;
