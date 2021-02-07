d1356fbd9d8818ffa370980ec253759e9820a963
Thomas Heritier <thomas.heritier@miles.no>
2021-02-07 18:56:27 +0100

    add factor to parallax background layer

--- components/parallax/ParallaxBackgroundLayer.js
+++ components/parallax/ParallaxBackgroundLayer.js
@@ -2,10 +2,11 @@ import React from 'react';
 import t from 'prop-types';
 import { ParallaxLayer } from 'react-spring/renderprops-addons';
 
-const ParallaxBackgroundLayer = ({ offset, speed, imgSrc }) => (
+const ParallaxBackgroundLayer = ({ offset, speed, factor = 1, imgSrc }) => (
   <ParallaxLayer
     offset={offset}
     speed={speed}
+    factor={factor}
     style={{
       backgroundImage: `url(${imgSrc})`,
       backgroundSize: 'cover'
@@ -16,6 +17,7 @@ const ParallaxBackgroundLayer = ({ offset, speed, imgSrc }) => (
 ParallaxBackgroundLayer.propTypes = {
   offset: t.number.isRequired,
   speed: t.number.isRequired,
+  factor: t.number,
   imgSrc: t.string.isRequired
 };
 
