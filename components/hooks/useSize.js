4a7789f2516503e1e166fc191a28ea1d82fd88c0
Thomas Heritier <thomas.heritier@miles.no>
2021-03-13 17:26:08 +0100

    change to useEffect for useSize hook

--- components/hooks/useSize.js
+++ components/hooks/useSize.js
@@ -1,11 +1,11 @@
-import { useState, useLayoutEffect } from 'react';
+import { useState, useEffect } from 'react';
 import useResizeObserver from '@react-hook/resize-observer';
 
 const useSize = (target) => {
   const [size, setSize] = useState();
 
-  useLayoutEffect(() => {
-    setSize(target.current?.getBoundingClientRect());
+  useEffect(() => {
+    setSize(target?.current?.getBoundingClientRect());
   }, [target]);
 
   useResizeObserver(target, (entry) => setSize(entry.contentRect));
