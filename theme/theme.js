b046a6136a21764410bb3c7f9c3cbb1b3f4605fb
Thomas Heritier <thomas.heritier@miles.no>
2021-03-14 12:47:03 +0100

    add new font

--- theme/theme.js
+++ theme/theme.js
@@ -6,48 +6,48 @@ const theme = {
   sizes,
   space,
   fonts: {
-    heading: '"Inter Regular"',
-    body: '"Inter Regular"',
-    semibold: '"Inter Semibold"',
-    regular: '"Inter Regular"',
-    medium: '"Inter Medium"',
-    italic: '"Inter Italic"',
-    italicHeading: '"Inter Medium Italic"',
+    heading: '"Amatic Regular"',
+    body: '"Amatic Regular"',
+    semibold: '"Amatic Regular"',
+    regular: '"Amatic Regular"',
+    medium: '"Amatic Regular"',
+    italic: '"Amatic Regular"',
+    italicHeading: '"Amatic Regular"',
   },
   fontFaces: {
     heading: {
-      name: 'Inter Regular',
-      url: '/fonts/Inter-Regular.woff2',
+      name: 'Amatic Regular',
+      url: '/fonts/amatic-regular.woff2',
       format: 'woff2'
     },
     body: {
-      name: 'Inter Regular',
-      url: '/fonts/Inter-Regular.woff2',
+      name: 'Amatic Regular',
+      url: '/fonts/amatic-regular.woff2',
       format: 'woff2'
     },
     semibold: {
-      name: 'Inter Semibold',
-      url: '/fonts/Inter-SemiBold.woff2',
+      name: 'Amatic Regular',
+      url: '/fonts/amatic-regular.woff2',
       format: 'woff2'
     },
     medium: {
-      name: 'Inter Medium',
-      url: '/fonts/Inter-Medium.woff2',
+      name: 'Amatic Regular',
+      url: '/fonts/amatic-regular.woff2',
       format: 'woff2'
     },
     regular: {
-      name: 'Inter Regular',
-      url: '/fonts/Inter-Regular.woff2',
+      name: 'Amatic Regular',
+      url: '/fonts/amatic-regular.woff2',
       format: 'woff2'
     },
     italic: {
-      name: 'Inter Italic',
-      url: '/fonts/Inter-Italic.woff2',
+      name: 'Amatic Regular',
+      url: '/fonts/amatic-regular.woff2',
       format: 'woff2'
     },
     italicHeading: {
-      name: 'Inter Medium Italic',
-      url: '/fonts/Inter-MediumItalic.woff2',
+      name: 'Amatic Regular',
+      url: '/fonts/amatic-regular.woff2',
       format: 'woff2'
     }
   },
