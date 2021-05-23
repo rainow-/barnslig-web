abdd3f5b482a6730281c1c17dfef94e10bab6b82
Thomas Heritier <thomas.heritier@miles.no>
2021-05-23 22:47:58 +0200

    stuffz

--- components/ComingSoon.js
+++ components/ComingSoon.js
@@ -6,25 +6,25 @@ import Grid from './common/Grid';
 import Img from './common/Img';
 import Section from './common/Section';
 
-const AnimatedBox = motion.custom(Box);
+const AnimatedImg = motion.custom(Img);
 
 const Hero = () => {
   const variants = {
     expand: {
       scale: [1, 1.2],
-      transition: { duration: 1, repeat: Infinity, repeatType: 'reverse' }
+      transition: { duration: 0.8, repeat: Infinity, repeatType: 'reverse' }
     }
   };
 
   return (
     <Section mx="auto">
-      <Box position="absolute" left={["50%", 0]}>
+      <Box position="absolute" left={['50%', 0]}>
         <Img
           src="/images/logo.png"
           alt="Logo"
           maxWidth={[200]}
           position="relative"
-          left={["-50%", 10]}
+          left={['-50%', 10]}
           top={[10]}
         />
       </Box>
@@ -34,17 +34,21 @@ const Hero = () => {
           maxW="3xl"
           templateColumns="repeat(16, 1fr)"
           templateRows="repeat(16, 1fr)">
-          <Box gridArea="3 / 2 / 17 / 16">
+          <Box gridArea="3 / 2 / 17 / 16" zIndex={10}>
             <Img src="/images/burger.png" alt="Burger" width="100%" height="100%" />;
           </Box>
-          <AnimatedBox
-            variants={variants}
-            animate="expand"
-            position="absolute"
+          <Box
             gridArea={['1 / 9 / 5 / 17', '2 / 10 / 5 / 16']}
             style={{ rotate: 5, willChange: 'transform' }}>
-            <Img src="/images/coming-soon.png" alt="Coming Soon!" width="100%" height="100%" />
-          </AnimatedBox>
+            <AnimatedImg
+              variants={variants}
+              animate="expand"
+              src="/images/coming-soon.png"
+              alt="Coming Soon!"
+              width="100%"
+              height="100%"
+            />
+          </Box>
         </Grid>
       </Flex>
     </Section>
